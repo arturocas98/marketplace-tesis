bd2cea74837844e4161a2c869e58e9be7fbf4d2a
arturocas98 <arturocastro98@hotmail.com>
2020-12-02 20:46:57 -0500

    app.component.ts terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 declare var jQuery:any;
 declare var $:any;
-declare var StickySidebar:any;
+// declare var StickySidebar:any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -632,31 +632,31 @@ export class AppComponent implements OnInit {
         });
       }
 
-      function stickySidebar() {
-        var sticky = $('.ps-product--sticky'),
-          stickySidebar, checkPoint = 992,
-          windowWidth = $(window).innerWidth();
-        if (sticky.length > 0) {
-          stickySidebar = new StickySidebar('.ps-product__sticky .ps-product__info', {
-            topSpacing: 20,
-            bottomSpacing: 20,
-            containerSelector: '.ps-product__sticky',
-          });
-          if ($('.sticky-2').length > 0) {
-            var stickySidebar2 = new StickySidebar('.ps-product__sticky .sticky-2', {
-              topSpacing: 20,
-              bottomSpacing: 20,
-              containerSelector: '.ps-product__sticky',
-            });
-          }
-          if (checkPoint > windowWidth) {
-            stickySidebar.destroy();
-            stickySidebar2.destroy();
-          }
-        } else {
-          return false;
-        }
-      }
+      // function stickySidebar() {
+      //   var sticky = $('.ps-product--sticky'),
+      //     stickySidebar, checkPoint = 992,
+      //     windowWidth = $(window).innerWidth();
+      //   if (sticky.length > 0) {
+      //     stickySidebar = new StickySidebar('.ps-product__sticky .ps-product__info', {
+      //       topSpacing: 20,
+      //       bottomSpacing: 20,
+      //       containerSelector: '.ps-product__sticky',
+      //     });
+      //     if ($('.sticky-2').length > 0) {
+      //       var stickySidebar2 = new StickySidebar('.ps-product__sticky .sticky-2', {
+      //         topSpacing: 20,
+      //         bottomSpacing: 20,
+      //         containerSelector: '.ps-product__sticky',
+      //       });
+      //     }
+      //     if (checkPoint > windowWidth) {
+      //       stickySidebar.destroy();
+      //       stickySidebar2.destroy();
+      //     }
+      //   } else {
+      //     return false;
+      //   }
+      // }
 
       function accordion() {
         var accordion = $('.ps-accordion');
@@ -741,13 +741,13 @@ export class AppComponent implements OnInit {
         backToTop();
         stickyHeader();
         filterSlider();
-        mapConfig();
+        // mapConfig();
         modalInit();
         searchInit();
         countDown();
         mainSlider();
         parallax();
-        stickySidebar();
+        // stickySidebar();
         accordion();
         progressBar();
         customScrollbar();
